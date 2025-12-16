9869b41b9dd84a62508789c2a9b1d351d0287257
nifla-noushad <ayishanifla700@gmail.com>
2025-12-16 16:04:28 +0530

    removed

--- src/core/school/models.py
+++ src/core/school/models.py
@@ -3,11 +3,3 @@ from django.db import models
 # Create your models here.
 
 
-class Student(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField
-
-class Teacher(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField
-    subject = models.CharField(max_length=100)
