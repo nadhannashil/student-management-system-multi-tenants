acc874256a71f53f65444f1f270030b4c43ab6e5
nadhan <nadhannashil4@gmail.com>
2025-12-17 12:04:48 +0530

    created tenants models and middleware, modified settings.py

--- src/core/tenants/models.py
+++ src/core/tenants/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.module_loading import module_has_submodule
 
-# Create your models here.
+
+class Tenants(models.Model):
+    TENANT_TYPES = (
+        ('school', 'School'),
+        ('college', 'College'),
+        ('institution', 'Institution'),
+    )
+
+    name = models.CharField(max_length=200)
+    tenant_type = models.CharField(max_length=20, choices=TENANT_TYPES)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.name} ({self.tenant_type})"
